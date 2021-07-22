8571c287039a096ad1d0ac9b053c8dc25e3a4162
geedtd <grcazares@gmail.com>
2021-07-22 08:15:32 -0700

    deleted recipe controller, migrated code to recipes controller

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -25,3 +25,28 @@ function index(req, res) {
     
     
 }
+
+// import { Recipe } from "../models/recipe.js";
+
+// export {
+//     newRecipe as new,
+//     create,
+
+// }
+
+
+// function create(req, res) {
+//     req.body.owner = req.user.profile 
+//     Recipe.create(req.body)
+//     .then(recipe => {
+//         res.redirect('/recipes')
+//     })
+//     .catch(err => {
+//         console.log(err)
+//         res.redirect('/recipes')
+//     })
+// }
+
+// function newRecipe(req, res) {
+//     res.render('newRecipe/new', { title: 'New Recipe', user: req.user ? req.user : null })
+// }
