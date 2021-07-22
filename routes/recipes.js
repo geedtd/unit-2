4514eedd0dbe80bad32a097dd7a502ce2c9793b0
geedtd <grcazares@gmail.com>
2021-07-22 07:47:14 -0700

    going to delete newRecipe, no point in having that whole MVC if I can just add on the recipe MVC

--- routes/recipes.js
+++ routes/recipes.js
@@ -7,3 +7,17 @@ router.get('/',recipesCtrl.index)
 export {
     router
 }
+
+
+// import { Router } from 'express'
+// const router = Router()
+// import * as newRecipeCtrl from '../controllers/newRecipe.js'
+// import { isLoggedIn } from '../middleware/middleware.js'
+
+// export {
+//     router
+// }
+
+
+// router.get('/new', newRecipeCtrl.new)
+// router.post('/', isLoggedIn, newRecipeCtrl.create)
