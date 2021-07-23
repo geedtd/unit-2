612b6a21c19ab88666889d97427150772e289c62
geedtd <grcazares@gmail.com>
2021-07-22 21:53:07 -0700

    created a show view for profiles

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -6,7 +6,23 @@ export {
 }
 
 function show(req, res) {
-    console.log('work please')
+    Profile.findById(req.parms.id)  
+    .then((profile) => {
+        Profile.findById(req.user.profile._id)
+        .then( self => {
+            const isSelf = self._id.equals(profile.__id)
+            res.render('profiles/show', {
+                title: `Chef ${profile.name}'s Profile`,
+                profile,
+                self,
+                isSelf,
+            })
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect(`/profiles/${req/user.profile}`)
+    })
 }
 
 function index(req, res) {
